8e114defcf57ded7a376d01c94bc3771c167877d
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-01-13 17:32:31 -0500

    fix handlers argument

--- Todos/handlers.py
+++ Todos/handlers.py
@@ -1,29 +1,29 @@
 from aiohttp.web import Response
 
 
-def get_all_todos():
+def get_all_todos(request):
     return Response(text='get_all_todos')
 
 
-def create_todos():
+def create_todos(request):
     return Response(text='create_todos')
 
 
-def update_todos():
+def update_todos(request):
     return Response(text='update_todos')
 
 
-def remove_todos():
+def remove_todos(request):
     return Response(text='remove_todos')
 
 
-def get_todo():
+def get_todo(request):
     return Response(text='get_todo')
 
 
-def update_todo():
+def update_todo(request):
     return Response(text='update_todo')
 
 
-def remove_todo():
+def remove_todo(request):
     return Response(text='remove_todo')
